1995de34a2627f549df05396b34106d39deda071
Vincent CY Liao <che_yin.liao@iisigroup.com>
2023-09-10 02:46:04 +0800

    update sample http service

--- sample/main.go
+++ sample/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"flag"
 	"log"
 	"net/http"
@@ -9,17 +10,21 @@ import (
 	httpservefile "github.com/yinyin/go-util-http-serve-file"
 )
 
-func parseCommandParam() (httpAddr, contentFolderPath, contentZipPath, contentStorePrefix, targetRelPath string) {
+//go:embed test-data
+var contentRef embed.FS
+
+func parseCommandParam() (httpAddr, contentFolderPath, contentZipPath string, contentEmbed bool, contentStorePrefix, targetRelPath string) {
 	flag.StringVar(&httpAddr, "listen", ":8080", "port and address to listen on")
 	flag.StringVar(&contentFolderPath, "folder", "", "path to content folder")
 	flag.StringVar(&contentZipPath, "zip", "", "path to content zip archive")
+	flag.BoolVar(&contentEmbed, "embed", false, "use embedded blob as content")
 	flag.StringVar(&contentStorePrefix, "prefix", "", "content prefix path in content store")
 	flag.StringVar(&targetRelPath, "target", "", "relative path to target file")
 	flag.Parse()
 	return
 }
 
-func setupContentServer(contentFolderPath, contentZipPath, contentStorePrefix string) httpservefile.HTTPFileServer {
+func setupContentServer(contentFolderPath, contentZipPath string, contentEmbed bool, contentStorePrefix string) httpservefile.HTTPFileServer {
 	switch {
 	case contentFolderPath != "":
 		srv, err := httpservefile.NewServeFileSystemWithPrefix(contentURLPathPrefix, contentFolderPath)
@@ -37,6 +42,17 @@ func setupContentServer(contentFolderPath, contentZipPath, contentStorePrefix st
 		}
 		log.Printf("INFO: setup content server with ZipArchive: [%s::%s]", contentZipPath, contentStorePrefix)
 		return srv
+	case contentEmbed:
+		if contentStorePrefix != "test-data" {
+			log.Printf("WARN: content store prefix (option `-prefix`) not set to `test-data`: [%s]", contentStorePrefix)
+		}
+		srv, err := httpservefile.NewServeFSWithPrefix(contentURLPathPrefix, contentRef, contentStorePrefix)
+		if nil != err {
+			log.Fatalf("ERROR: failed on setting up content server with fs.FS [%s]: %v", contentStorePrefix, err)
+			return nil
+		}
+		log.Printf("INFO: setup content server with fs.FS: [%s]", contentStorePrefix)
+		return srv
 	default:
 		log.Printf("WARN: empty content store")
 	}
@@ -44,10 +60,10 @@ func setupContentServer(contentFolderPath, contentZipPath, contentStorePrefix st
 }
 
 func main() {
-	httpAddr, contentFolderPath, contentZipPath, contentStorePrefix, targetRelPath := parseCommandParam()
+	httpAddr, contentFolderPath, contentZipPath, contentEmbed, contentStorePrefix, targetRelPath := parseCommandParam()
 	log.Printf("INFO: listen on address: [%s]", httpAddr)
 	h := &sampleHandler{
-		contentServer:        setupContentServer(contentFolderPath, contentZipPath, contentStorePrefix),
+		contentServer:        setupContentServer(contentFolderPath, contentZipPath, contentEmbed, contentStorePrefix),
 		targetContentRelPath: targetRelPath,
 	}
 	defer h.Close()
